3ba694c8984cdaa2984dba1bda63d7bed078fb0b
seo9204 <seo9204@gmail.com>
2019-08-11 17:06:23 -0400

    add backend work

--- UserModel.js
+++ UserModel.js
@@ -10,6 +10,7 @@ UserModel.addUser = function(email, password, callback){
         callback(false, "Email is ill-formed");
     }
     else if(email in users){
+        console.log(email, users);
         callback(false, "Email already exists");
     }
     else if(!password){
@@ -21,5 +22,26 @@ UserModel.addUser = function(email, password, callback){
     }
 };
 
-module.exports = UserModel;
+UserModel.login = function(email, password, callback) {
+  if (email in users) {
+    console.log(users[email].password, password);
+    if (users[email].password === password) {
+        return callback(true, "authenticated")
+    }
+  }
+  callback(false, "invalid credentials")
+}
 
+UserModel.addSecret = function(email, secret) {
+  users[email].secret = secret;
+  console.log('secret',email, secret);
+}
+
+UserModel.guessSecret = function(email, secret, callback) {
+  if (email in users && users[email].secret) {
+      users[email].secret === secret ? callback(true, "matched") : callback(false, "not matched")
+  } else {
+    callback(false, "not found")
+  }
+}
+module.exports = UserModel;
--- routes.js
+++ routes.js
@@ -1,9 +1,10 @@
-require('dotenv').config()
-var express = require('express');
-var bodyParser = require('body-parser');
-var jwt = require('jsonwebtoken');
-var User = require('./UserModel');
-var JWT_SECRET = process.env.JWT_SECRET;
+require('dotenv').config({ path: './test.env' })
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const User = require('./UserModel');
+const bcrypt = require('bcryptjs');
+const JWT_SECRET = process.env.JWT_SECRET;
 
 
 var app = express();
@@ -19,37 +20,70 @@ app.use(bodyParser.json());
 
 
 app.post('/user/createUser', function(req, res){
-            var email = req.body.email;
-            var password = req.body.password;
-            console.log(email,password);
-            User.addUser(email, password, function(addSuccess, errMsg){
-                if (!addSuccess)
-                    return res.status(400).send({message: errMsg });
-                else {
-                    var token = jwt.sign(
-                        {id: email}, 
-                        JWT_SECRET, {
-                        expiresIn: '24h',
-                        });
-                    return res.status(200).send({token});
-                }
-            });
-            
+          var email = req.body.email;
+          var password = req.body.password;
+          console.log(email,password);
+          User.addUser(email, password, function(addSuccess, errMsg){
+              if (!addSuccess)
+                  return res.status(400).send({message: errMsg });
+              else {
+                  var token = jwt.sign(
+                      {id: email},
+                      JWT_SECRET, {
+                      expiresIn: '24h',
+                      });
+                  return res.status(200).send({token});
+              }
+          });
 });
 
 //TODO
 app.post('/user/login', function(req, res){
-    return res.status(500).send("Not Implemented");
+  var email = req.body.email;
+  var password = req.body.password;
+  User.login(email, password, function(sucesss, err) {
+      if (!sucesss) {
+        return res.status(403).send({message: err})
+      } else {
+        var token = jwt.sign(
+            {id: email},
+            JWT_SECRET, {
+            expiresIn: '24h',
+            });
+        return res.status(200).send({token})
+      }
+  })
 });
 
 //TODO
 app.put('/user/addSecret', function(req, res){
-    return res.status(500).send("Not Implemented");
+  var secret = req.body.secret;
+  var token = req.body.token;
+    jwt.verify(token, secret, function(err, decoded) {
+      if (err) {
+        console.log(err);
+        return res.status(403).send(err);
+      } else {
+        User.addSecret(decoded.id, secret);
+        return res.status(200).send(decoded)
+    }
+  });
 });
 
 //TODO
 app.get('/user/guessSecret', function(req, res){
-    return res.status(500).send("Not Implemented");
+  var email = req.body.email;
+  var secret = req.body.secret;
+  User.guessSecret(email, secret, function(success, msg) {
+    if (success) {
+      return res.status(200).send(success)
+    } else {
+      if (msg === "not matched") {
+        return res.status(200).send(success)
+      }
+      return res.status(404).send({message: msg})
+    }
+  })
 });
 
 
